2ba989db0723285174da6e6ab40bb4b61ee872ab
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2020-02-19 13:54:21 -0800

    Issues with base case and zero

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -7,19 +7,28 @@ import sys
 
 def rock_paper_scissors(n):
   plays = ['rock', 'paper', 'scissors']
-  # Multiple recurssive calls so that each call can 
-  # add to the result.
-  # if n == 0:
-  #   return [[]]
-  # if n == 1:
-  #   #list length can only be 1 + 1 + 1
-  #   return [[plays[0]]] + [[plays[1]]] + [[plays[2]]]
-  # if n == 2:
-  #   #list length can only be 2 + 2 + 2
-  #   print( [[plays[0]] + [plays[0]]] + [[plays[1]] + [plays[1]]] + [[plays[2]] + [plays[2]]] + [[plays[0]] + [plays[1]]] + [[plays[0]] + [plays[2]]] + [[plays[1]] + [plays[2]]] + [[plays[1]] + [plays[0]]] + [[plays[2]] + [plays[0]]] + [[plays[2]] + [plays[1]]] )
-  #   return [[plays[0]] + [[plays[0]]]] + [[plays[1]] + [plays[1]]] + [[plays[2]] + [plays[2]]] + [[plays[0]] + [plays[1]]] + [[plays[0]] + [plays[2]]] + [[plays[1]] + [plays[2]]] + [[plays[1]] + [plays[0]]] + [[plays[2]] + [plays[0]]] + [[plays[2]] + [plays[1]]]
-  #   #n = list length
   
+  possibilites = []
+
+  #Recursive Case
+  def choice(round, num):
+    #Base Case
+    for i in range(0, len(plays)):
+      round.append(plays[i])
+      if num >= n:
+        possibilites.append(round[0:len(round)]
+      elif n == 0:
+        return possibilites.append([])
+      else:
+        #Progress made at each invocation of the 
+        # recursive function.
+        choice(round, num + 1)
+      round.pop()
+  
+  choice([],1)
+  return possibilites
+
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
